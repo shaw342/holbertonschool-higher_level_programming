9677aa459f67266f4c894c3062e1be109491c742
shaw342 <baruashawan41@gmail.com>
2023-05-31 00:36:10 +0200

    refactor by one loop for

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for y in range(0,10):
-        if i == 9 and y == 9:
-            print(f"{i}{y}")
-            break;
-        print(f"{i}{y}, ",end='')
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}, ".format(i), end='')
+    else:
+        print("{:02d}".format(i))
