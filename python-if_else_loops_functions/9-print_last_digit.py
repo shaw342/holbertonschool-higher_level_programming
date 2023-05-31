cdbbbdf246ccc4ccc3350d74fe212dc291c4d90d
shaw342 <baruashawan41@gmail.com>
2023-05-31 21:45:00 +0200

    pycode

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if number >= 0:
         lastdigit = number % 10
     else:
-        lastdigit = abs(number) % 10 
+        lastdigit = abs(number) % 10
     print("{}".format(lastdigit), end="")
     return lastdigit
