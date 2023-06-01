a1cc282c212bdd1ae26587a94ca803d0f8dafcbd
shaw342 <baruashawan41@gmail.com>
2023-06-02 01:49:54 +0200

    test3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 argv = sys.argv
 if (len(argv) - 1) == 1:
-    print("{} argument".format(len(argv) - 1))
+    print("{} argument:".format(len(argv) - 1))
 else:
     print("{} arguments:".format(len(argv) - 1))
 for i in range(1, len(argv)):
