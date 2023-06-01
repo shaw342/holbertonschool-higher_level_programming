3150144e3b1e2c58559fd064eec90d4aa2559146
shaw342 <baruashawan41@gmail.com>
2023-06-02 01:59:10 +0200

    test5

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     argv = sys.argv
     if (len(argv) - 1) == 1:
         print("{} argument:".format(len(argv) - 1))
+    elif(len(argv) - 1) == 0:
+        print("{} argument:".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
     for i in range(1, len(argv)):
