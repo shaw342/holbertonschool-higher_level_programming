7cd984a778c78916d53ab286dc5aadffa4e4b8dc
shaw342 <baruashawan41@gmail.com>
2023-06-02 01:42:03 +0200

    test2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import sys
 argv = sys.argv
-count = 0
-print("{} arguments:".format(len(argv) - 1))
+if (len(argv) - 1) == 1:
+    print("{} argument".format(len(argv) - 1))
+else:
+    print("{} arguments:".format(len(argv) - 1))
 for i in range(1, len(argv)):
     print("{}: {}".format(i, sys.argv[i]))
