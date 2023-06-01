c409fdb7c3686334e2bc96c853d3d35f381e6c1c
shaw342 <baruashawan41@gmail.com>
2023-06-02 01:35:48 +0200

    print argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,4 +4,4 @@ argv = sys.argv
 count = 0
 print("{} arguments:".format(len(argv) - 1))
 for i in range(1, len(argv)):
-    print("{}:{}".format(i, sys.argv[i]))
+    print("{}: {}".format(i, sys.argv[i]))
