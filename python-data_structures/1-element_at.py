8aac1d2d7e47d71fcc305ab9ae8ffbfc0013d18b
shaw342 <baruashawan41@gmail.com>
2023-06-02 23:45:07 +0200

    forget condition

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif not in range(len(my_list)):
+    elif idx not in range(len(my_list)):
         return None
     return my_list[idx]
