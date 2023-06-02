6b0613899de0e4fb07d769b15190ed0a48c1ef48
shaw342 <baruashawan41@gmail.com>
2023-06-03 00:42:38 +0200

    newList

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
 
     if idx < 0:
-        return
+        return my_list
     if idx not in range(len(my_list)):
-        return
+        return my_list
     new_list = my_list[:]
     new_list[idx] = element
     return new_list
