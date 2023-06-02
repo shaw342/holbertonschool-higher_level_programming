00a26e8a31b59907f478f07ab61e8f92ea64ce79
shaw342 <baruashawan41@gmail.com>
2023-06-03 01:26:54 +0200

    refactor the code

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string=''
-    if my_string:
-        for i in range(len(my_string)):
-            if my_string[i] != 'c' and my_string[i] != 'C':
-                new_string += my_string[i]
+    new_string=""
+        for i in my_string:
+            if i != 'C' and i != 'c':
+                new_string += i
 
     return new_string
