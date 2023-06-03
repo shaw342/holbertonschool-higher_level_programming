005007d605c350265a2496ceadde78968c851c7d
shaw342 <baruashawan41@gmail.com>
2023-06-03 07:06:27 +0200

    pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return len(sentence),None
+        return len(sentence), None
     a = len(sentence)
-    return a,sentence[0]
+    return a, sentence[0]
