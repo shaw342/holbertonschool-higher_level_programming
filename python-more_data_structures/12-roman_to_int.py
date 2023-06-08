ea9e5272e0d692f9e4bb18711d57890ee74b5db9
shaw342 <baruashawan41@gmail.com>
2023-06-08 02:51:28 +0200

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,12 @@
 def roman_to_int(chaine_romaine):
     if not isinstance(chaine_romaine, str) or chaine_romaine is None:
         return 0
-    
     chiffres_romains = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-    
     resultat = 0
     valeur_precedente = 0
-    
     for caractere in reversed(chaine_romaine):
         valeur = chiffres_romains.get(caractere, 0)
         if valeur >= valeur_precedente:
@@ -18,5 +15,4 @@ def roman_to_int(chaine_romaine):
         else:
             resultat -= valeur
         valeur_precedente = valeur
-    
     return resultat
