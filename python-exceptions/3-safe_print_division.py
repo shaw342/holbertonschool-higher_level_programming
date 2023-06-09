c0412192df59eba4eccf1efc43f51586ebbb1417
shaw342 <baruashawan41@gmail.com>
2023-06-09 16:43:15 +0200

    task3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        return a / b
+    except:
+        return
+    finally:
+        if b == 0:
+            print("Inside result:" + str(None))
+        else:
+            print("Inside result: {}".format(a / b))
