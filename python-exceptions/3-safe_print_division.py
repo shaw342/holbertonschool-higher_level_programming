4cd9446e4e1ba9b4f9949eac4e3855fc24eda43c
shaw342 <baruashawan41@gmail.com>
2023-06-09 16:48:24 +0200

    task3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,10 +2,10 @@
 def safe_print_division(a, b):
     try:
         return a / b
-    except:
+    except ZeroDivisionError:
         return
     finally:
         if b == 0:
-            print("Inside result:" + str(None))
+            print("Inside result: " + str(None))
         else:
-            print("Inside result: {}".format(a / b))
+            print("Inside result: " + "{}".format(a / b))
