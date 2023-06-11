e644aa080f5485538d355dc3df1e72ded71826cb
shaw342 <baruashawan41@gmail.com>
2023-06-11 06:12:59 +0200

    task 13

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == 0:
+        return 0
+    total_score = 0
+    total_weight = 0
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    average_weight = total_score / total_weight
+    return average_weight
