57b5c2e5b47bb0517150ba6f5d05a623def841e8
shaw342 <baruashawan41@gmail.com>
2023-06-11 23:04:22 +0200

    task 101 don't use variable'

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    matrix = map(lambda ligne: list(map(lambda x: x ** 2, ligne)), matrix)
-    return list(matrix)
+    return list(map(lambda ligne: list(map(lambda x: x ** 2, ligne)), matrix))
