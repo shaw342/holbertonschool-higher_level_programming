3c814c8bf5a924efaa973c01965d2d29f834a0e4
shaw342 <baruashawan41@gmail.com>
2023-06-12 13:40:31 +0200

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""define square class"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of Square"""
+        return self.__size * self. __size
+
+    @property
+    def size(self):
+        """Size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
