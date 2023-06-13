8a231469bfb029212282d499515aaf64c842dfe7
VotreNom <shawanbarua70@gmail.com>
2023-06-13 19:05:58 +0200

    Your commit message

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    result = []
+
+    if type(Matrix) is not float or int:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if type(div) is not float or int:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for i in range()
+
+    return result
