882ff9f9d54fc24a52a6005839234a17a0f37fa2
VotreNom <shawanbarua70@gmail.com>
2023-06-15 13:22:29 +0200

    task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""define function say my name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """function say my name"""
 
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
@@ -7,4 +11,4 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    print("My name is {:s} {:s}".format(first_name,last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
