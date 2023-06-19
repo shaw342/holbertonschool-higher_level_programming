6a5dfa1dd6e20c4d966bfe3bf969bd7abb38fa3b
VotreNom <shawanbarua70@gmail.com>
2023-06-19 13:23:14 +0200

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""define class"""
+
+
+class MyList(list):
+    """MyList """
+
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        print(sorted(self))
