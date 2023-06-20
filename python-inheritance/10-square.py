00fc8e7f57ccc1e9ebd84e318c3c61a18b2363af
VotreNom <shawanbarua70@gmail.com>
2023-06-20 15:10:45 +0200

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""define class"""
+
+
+class Square(Rectangle):
+    """ Base Geometry """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
