fb031f0129473c985511ecad7416ea6c256d52a6
VotreNom <shawanbarua70@gmail.com>
2023-06-20 15:19:22 +0200

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""define class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Base Geometry """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """define area function"""
+        return self.__size ** 2
+    
+    def __str__(self):
+        return f"[Square] {self.__size} {self.__size}"
