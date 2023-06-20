36de5df0233acf122aa77b1ff0580b8e8b799c6e
VotreNom <shawanbarua70@gmail.com>
2023-06-20 15:22:10 +0200

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,6 +13,6 @@ class Square(Rectangle):
     def area(self):
         """define area function"""
         return self.__size ** 2
-    
+
     def __str__(self):
-        return f"[Square] {self.__size} {self.__size}"
+        return "[Square] {}/{}".format(self.__size, self.__size)
