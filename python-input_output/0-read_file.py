a2a8aa351bea4333fc292407390cf68b8fd82307
VotreNom <shawanbarua70@gmail.com>
2023-06-21 15:28:31 +0200

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""define function read"""
+
 
 def read_file(filename=""):
+    """read file function"""
     with open(filename, encoding="utf-8") as f:
         f = (f.read()[:-1])
-        if f == ' ':
+        if f == "":
             return
         else:
             print(f)
