4454a2d1e2569b1bded761d79083f695150102d6
VotreNom <shawanbarua70@gmail.com>
2023-06-23 12:48:15 +0200

    task6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import json
+
+
 def load_from_json_file(filename):
-    with open(filename,'r') as f:
-        new_obj = json.load(f)
-        return new_obj
+
+    with open(filename, 'r') as f:
+        return json.load(f)
