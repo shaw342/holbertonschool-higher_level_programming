e5e706c7b4920237822435f5f06e890b14eabe0d
VotreNom <shawanbarua70@gmail.com>
2023-06-28 11:49:46 +0200

    modify message error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,7 @@ class Rectangle(Base):
     def width(self, value):
         """ width check  value """
         if type(value) is not int:
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
