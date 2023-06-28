bf889efcde361459e565a8d13708b8f524cbdaa2
VotreNom <shawanbarua70@gmail.com>
2023-06-28 22:40:16 +0200

    return strings

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,5 @@ class Rectangle(Base):
         """display rectangle"""
         for i in range(self.__height):
             print("#" * self.__width)
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y } - {self.__width}/{self.__height}"
