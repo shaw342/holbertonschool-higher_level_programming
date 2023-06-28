2d410c645450ef230f4850e9e03c20e2c3f696b2
VotreNom <shawanbarua70@gmail.com>
2023-06-28 12:24:07 +0200

    area

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,8 @@ class Rectangle(Base):
     def area(self):
         """area of rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """display rectangle"""
+        for i in range(self.__height):
+            print("#" * self.__width)
