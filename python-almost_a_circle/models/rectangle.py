6c3f56a1ee711bab36348e9a72f1ba058175d584
VotreNom <shawanbarua70@gmail.com>
2023-06-28 10:44:08 +0200

    forget assigne value

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -51,6 +51,7 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if value <= 0:
             raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
@@ -64,3 +65,4 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value <= 0:
             raise ValueError("y must be >= 0")
+        self.__y = value
