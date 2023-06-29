92ce90be51c984d30a073f0e16f8daa8378175c9
VotreNom <shawanbarua70@gmail.com>
2023-06-29 14:58:19 +0200

    forget doc

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""define Square"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
+    """classe square"""
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         super().__init__(size, size, x, y, id)
