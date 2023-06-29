b7069d0a98ebc4e580283c6fca296f4e60db06dc
VotreNom <shawanbarua70@gmail.com>
2023-06-29 15:06:49 +0200

    task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,3 +24,17 @@ class Square(Rectangle):
         """size setter"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ update """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.size = args[1]
+        if len(args) == 4:
+            self.x = args[3]
+        if len(args) == 5:
+            self.y = args[4]
