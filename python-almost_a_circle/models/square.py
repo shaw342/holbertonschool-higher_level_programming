c314697b6b4ab2b14e5549db01853ffeca54e8fe
VotreNom <shawanbarua70@gmail.com>
2023-06-29 15:14:37 +0200

    change conditio,

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,9 +32,9 @@ class Square(Rectangle):
 
         if len(args) >= 1:
             self.id = args[0]
-        if len(args) == 2:
+        if len(args) >= 2:
             self.size = args[1]
-        if len(args) == 3:
+        if len(args) >= 3:
             self.x = args[2]
-        if len(args) == 4:
+        if len(args) >= 4:
             self.y = args[3]
