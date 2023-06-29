a6afec927b579b88ba8a9143f4d327d4c687ca2f
VotreNom <shawanbarua70@gmail.com>
2023-06-29 15:59:26 +0200

    ls

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ json to string """
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
