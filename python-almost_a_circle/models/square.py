985ff68bca0902acb07cbc20b53f368baa552a9c
VotreNom <shawanbarua70@gmail.com>
2023-06-29 15:08:22 +0200

    change index and position

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -34,7 +34,7 @@ class Square(Rectangle):
             self.id = args[0]
         if len(args) == 2:
             self.size = args[1]
+        if len(args) == 3:
+            self.x = args[2]
         if len(args) == 4:
-            self.x = args[3]
-        if len(args) == 5:
-            self.y = args[4]
+            self.y = args[3]
