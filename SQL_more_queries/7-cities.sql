909716fae5ce6dbe25b8b930e22e6f0b2a1c47ac
VotreNom <shawanbarua70@gmail.com>
2023-07-25 08:35:51 +0200

    change oder

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,8 +4,8 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (id),
-	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
 	FOREIGN KEY (state_id)
 	REFERENCES states(id)
+	name VARCHAR(256) NOT NULL,
 );
