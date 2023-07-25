d3a3033cfbdfa1ec0255f4b7dde2e28df6b35eac
VotreNom <shawanbarua70@gmail.com>
2023-07-25 07:19:38 +0200

    task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL serve
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id)
+	REFERENCES states(id)
+);
