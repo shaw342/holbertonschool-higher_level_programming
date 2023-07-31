7ebc6dd580a899595b282fdedb502d10c5a657c2
VotreNom <shawanbarua70@gmail.com>
2023-08-01 00:36:43 +0200

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,12 +3,15 @@
 import MySQLdb as MC
 import sys
 
+
 if __name__ == "__main__":
     state_name = sys.argv[4]
-    conn = MC.connect(port=3306, user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    conn = MC.connect(port=3306, user=sys.argv[1],
+                      password=sys.argv[2], db=sys.argv[3])
 
     with conn.cursor() as cursor:
-        query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id"
+        query = "SELECT * FROM states WHERE\
+            BINARY name = %s ORDER BY states.id"
         cursor.execute(query, (state_name,))
         my_list = cursor.fetchall()
         for x in my_list:
