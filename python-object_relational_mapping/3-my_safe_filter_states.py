eb30610a9619e2df3f10f07e25cb2f8fa012b243
VotreNom <shawanbarua70@gmail.com>
2023-08-01 00:32:39 +0200

    refactor the code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
 """ Write a script that lists all states from the database hbtn_0e_0_usa"""
-import MySQLdb
+import MySQLdb as MC
 import sys
 
+if __name__ == "__main__":
+    state_name = sys.argv[4]
+    conn = MC.connect(port=3306, user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-state_name = sys.argv[4]
+    with conn.cursor() as cursor:
+        query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id"
+        cursor.execute(query, (state_name,))
+        my_list = cursor.fetchall()
+        for x in my_list:
+            print(x)
 
-conn = MySQLdb.connect(
-    host="localhost",
-    user=username,
-    password=password,
-    database=database
-)
-
-cursor = conn.cursor()
-
-query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id"
-
-cursor.execute(query, (state_name,))
-
-results = cursor.fetchall()
-
-# Traiter les résultats
-for row in results:
-    print(row)
-
-# Fermer le curseur et la connexion
-cursor.close()
-conn.close()
+    conn.close()
