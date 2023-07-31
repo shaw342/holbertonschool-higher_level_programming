5e6061dce23ac0f263801d14ef9617652d30fbe5
VotreNom <shawanbarua70@gmail.com>
2023-07-31 13:18:57 +0200

    drop the table

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,12 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
     conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=user, password=password, db=database_name)
+                           user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states;")
+    cursor.execute("SELECT DISTINCT * FROM states;")
     my_liste = cursor.fetchall()
     for x in my_liste:
         print(x)
+cursor.close()
+conn.close()
