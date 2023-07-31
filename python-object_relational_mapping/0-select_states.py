7a03beb86e7343c74b08f11f13f5103f0322b032
VotreNom <shawanbarua70@gmail.com>
2023-07-31 15:15:11 +0200

    pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" Write a script that lists all states from the database hbtn_0e_0_usa: pycode"""
+""" Write a script that lists all states from the database hbtn_0e_0_usa"""
 
 
-import MySQLdb
+import MySQLdb as MC
 import sys
 
-
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, \
-        user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        for row in cur.fetchall():
-            print(row)
+    conn = MC.connect(port=3306, user=sys.argv[1],
+                      password=sys.argv[2], db=sys.argv[3])
+    with conn.cursor() as cursor:
+        cursor.execute("select * from states order by states.id")
+        my_list = cursor.fetchall()
+        for x in my_list:
+            print(x)
     conn.close()
