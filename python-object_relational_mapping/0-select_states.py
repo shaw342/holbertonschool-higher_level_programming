2d2817f6162815c8df6a4a8358b92f848cfc56ab
VotreNom <shawanbarua70@gmail.com>
2023-07-31 15:03:31 +0200

    pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,10 +9,8 @@ import sys
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, \
         user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
-    cursor = conn.cursor()
-    cursor.execute("SELECT DISTINCT * FROM states ORDER BY id ASC;")
-    my_liste = cursor.fetchall()
-    for x in my_liste:
-        print(x)
-
-conn.close()
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        for row in cur.fetchall():
+            print(row)
+    conn.close()
