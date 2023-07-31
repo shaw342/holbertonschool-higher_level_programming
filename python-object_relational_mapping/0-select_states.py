ed03ec67a1a1acbec59cee40df7586cfe2c1cde7
VotreNom <shawanbarua70@gmail.com>
2023-07-31 13:21:04 +0200

    pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-
+"""Write a script that lists all states from the database hbtn_0e_0_usa:"""
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
