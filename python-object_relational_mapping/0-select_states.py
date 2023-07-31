21285127b3073e0298ba942a851e0313e4088f7a
VotreNom <shawanbarua70@gmail.com>
2023-07-31 14:59:55 +0200

    pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa: pycode"""
+
+
 import MySQLdb
 import sys
-"""Write a script that lists all states from the database hbtn_0e_0_usa:"""
+
+
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, \
+        user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
     cursor = conn.cursor()
-    cursor.execute("SELECT DISTINCT * FROM states;")
+    cursor.execute("SELECT DISTINCT * FROM states ORDER BY id ASC;")
     my_liste = cursor.fetchall()
     for x in my_liste:
         print(x)
-cursor.close()
+
 conn.close()
