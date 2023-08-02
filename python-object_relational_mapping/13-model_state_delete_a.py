c896609f95c63d44c86d1c84d46c268df7ca81ed
VotreNom <shawanbarua70@gmail.com>
2023-08-02 17:56:37 +0200

    task &

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa """
+"""States class"""
 
-import MySQLdb as MC
+from model_state import Base, State
 import sys
-from model_state import State, Base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(username, password, database))
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    States = session.query(State).filter(
-        State.name.contains('a')).all()
+    States = session.query(State).filter(State.name.contains('a')).all()
     for state in States:
         session.delete(state)
     session.commit()
+    
