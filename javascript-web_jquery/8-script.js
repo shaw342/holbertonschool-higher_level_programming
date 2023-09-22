2c39deb4ec5bcc624969f3e6ee7e506dbe4f6cdb
VotreNom <shawanbarua70@gmail.com>
2023-09-22 19:02:57 +0200

    task6789

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data){
+        for (const movie of data.results) {
+            $('ul#list_movies').append(`<li>${movie.title}</li>`);
+          }
+    });
+});
